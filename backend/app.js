61418dda02eb0e3aab2f6db81a8a629d62e07d5a
AneeshaMS <aneeshamsani@gmail.com>
2024-12-02 22:25:52 +0530

    new changes

--- backend/app.js
+++ backend/app.js
@@ -20,12 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "dist")));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/v1", indexRouter);
 app.use("/users", usersRouter);
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
