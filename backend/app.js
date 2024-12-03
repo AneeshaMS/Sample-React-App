907abee8e4ade376172357d3d077c1e99d89a278
AneeshaMS <aneeshamsani@gmail.com>
2024-12-03 18:23:16 +0530

    app.js changes

--- backend/app.js
+++ backend/app.js
@@ -10,17 +10,13 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+app.use(cors());
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
+app.use(logger("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "dist")));
-app.use(cors());
 
 app.use("/v1", indexRouter);
 app.use("/users", usersRouter);
